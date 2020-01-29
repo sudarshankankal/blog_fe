cd8f892657d07f04881023690ed477424215ff00
Sudarshan Kankal <sudarshan.kankal@zconsolutions.com>
2020-01-29 16:40:30 +0530

    added build

---
+++ build/precache-manifest.a6eb4c64a6c0cc591c521905c1034d0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de56a180e931024e9eefb7b98c8e69f5",
+    "url": "/blog_fe/index.html"
+  },
+  {
+    "revision": "9c452f63f4a626686a2c",
+    "url": "/blog_fe/static/js/2.99506aaf.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/blog_fe/static/js/2.99506aaf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9715bbb2a08fb9e0b4ad",
+    "url": "/blog_fe/static/js/main.492df104.chunk.js"
+  },
+  {
+    "revision": "aa438d77d6f56c27d27a",
+    "url": "/blog_fe/static/js/runtime-main.95d47907.js"
+  }
+]);
